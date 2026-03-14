9611b9ca2e26d5214b04fe52f64db6c07476d704
Johny Villegas <johny.villegas.dev@gmail.com>
2026-03-14 16:02:52 +0100

    se añade la informacion de la caja y el usuario en el header y se ajusta el formateo de la fecha

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -1,10 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import useCashStore from '../../store/useCashStore';
 import './../../css/components/common/PageHeader.css';
+import userStore from '../../store/userStore';
+import { formatDateToShow } from '../../helper/formatDate.helper';
 
 export const PageHeader = ({ title, nav = true }: { title: string, nav?: boolean }) => {
   const navigation = useNavigate();
+  const { cashBox } = useCashStore();
+  const { userData } = userStore();
   return (
     <div className="container">
+      <div className="cashbox-info">
+        <p>Apertura: {cashBox ? formatDateToShow(cashBox.opened_at) : "N/A"}</p>
+        <p>Usuario: {userData ? userData.name : "N/A"}</p>
+        <p>Rol: {userData ? userData.role : "N/A"}</p>
+      </div>
       {nav ? (
         <button className="btn-pos" onClick={() => navigation(-1)}>Ir atrás</button>
       ) : <div style={{ width: "75px" }} />}
--- src/helper/formatDate.helper.ts
+++ src/helper/formatDate.helper.ts
@@ -2,7 +2,7 @@ export const formatDateToShow = (dateString: string): string => {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
-    month: "long",
+    month: "short",
     day: "numeric",
     hour: "2-digit",
     minute: "2-digit",
